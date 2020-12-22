072179eb234978c3225c64545adfc9f8285cb902
John McDonald <jmcdonald1991@gmail.com>
2020-12-21 19:11:01 -0600

    enemy spawn and destroy, collisions

---
+++ Assets/EmitterHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitterHandler : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public float Timer = 1f;
+    public float interval = 1f;
+    public float xRange = 13.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateEnemies(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+        if (Timer <= 0f)
+        {
+            GameObject[] Enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            int count = Enemies.Length;
+            if (count <= 6)
+            {
+                float xPos = UnityEngine.Random.Range(-xRange, xRange);
+                GenerateEnemies(xPos);
+            }
+            Timer = interval;
+        }
+    }
+
+    private void GenerateEnemies(float xPos)
+    {
+        
+            prefab.GetComponent<Enemy>().notMoving = false;
+            Quaternion rotation = Quaternion.Euler(-90f, 0f, 0f);
+            Instantiate(prefab, new Vector3(xPos, gameObject.transform.localPosition.y, transform.localPosition.z), rotation);
+    }
+}
--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    int health = 100;
+    [SerializeField] int health = 100;
+    [SerializeField] float speed = 10f;
+    [SerializeField] bool invincible = false;
+    [SerializeField] public bool notMoving = false;
 
     // Start is called before the first frame update
     void Start()
@@ -15,14 +19,42 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!notMoving) {
+            MoveShip();
+        }
+    }
 
+    private void MoveShip()
+    {
+        float yPos = transform.localPosition.y;
+        yPos = yPos - speed * Time.deltaTime;
+        transform.localPosition = new Vector3(transform.localPosition.x, yPos, transform.localPosition.z);
     }
 
     void OnParticleCollision(GameObject other)
     {
-        health = health - 5;
-        if(health <= 0)
+        if(!invincible)
         {
+            TakeDamage();
+        }
+
+    }
+
+    private void TakeDamage()
+    {
+        health = health - 5;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            print("hit player");
+            Destroy(collision.gameObject);
             Destroy(gameObject);
         }
     }
